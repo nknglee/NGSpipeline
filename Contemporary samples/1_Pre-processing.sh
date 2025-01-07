34811814667b7327f0c7232e8baf56779a75e59b
nknglee <50165480+nknglee@users.noreply.github.com>
2025-01-07 11:43:01 +0000

    Update 1_Pre-processing.sh
    Activate command lines by removung '#'

--- Contemporary samples/1_Pre-processing.sh
+++ Contemporary samples/1_Pre-processing.sh
@@ -19,7 +19,7 @@ HOM=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/farmers
 
 #code
 #Quality check - FastQC
-#cat ${HOM}/sample_list | parallel --gnu -j 14 "fastqc ${DAT}/{}_1.fastq.gz ${DAT}/{}_2.fastq.gz -o ${HOM}/fastqc"
+cat ${HOM}/sample_list | parallel --gnu -j 14 "fastqc ${DAT}/{}_1.fastq.gz ${DAT}/{}_2.fastq.gz -o ${HOM}/fastqc"
 
 #Adapter/quality trimming - Trimmomatic
-#cat ${HOM}/sample_list | parallel --gnu -j 14 "trimmomatic PE -phred33 ${DAT}/{}_1.fastq.gz ${DAT}/{}_2.fastq.gz ${HOM}/trim/{}_1_paired.fastq.gz ${HOM}/trim/{}_1_unpaired.fastq.gz ${HOM}/trim/{}_2_paired.fastq.gz ${HOM}/trim/{}_2_unpaired.fastq.gz ILLUMINACLIP:./share/trimmomatic-0.39-2/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50"
+cat ${HOM}/sample_list | parallel --gnu -j 14 "trimmomatic PE -phred33 ${DAT}/{}_1.fastq.gz ${DAT}/{}_2.fastq.gz ${HOM}/trim/{}_1_paired.fastq.gz ${HOM}/trim/{}_1_unpaired.fastq.gz ${HOM}/trim/{}_2_paired.fastq.gz ${HOM}/trim/{}_2_unpaired.fastq.gz ILLUMINACLIP:./share/trimmomatic-0.39-2/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50"
