d4d71031a4090408e7ee44ff7c1b81678b275006
nknglee <50165480+nknglee@users.noreply.github.com>
2025-01-13 09:58:03 +0000

    Update 6_Post-processing.sh

--- Contemporary samples/6_Post-processing.sh
+++ Contemporary samples/6_Post-processing.sh
@@ -1,36 +1,40 @@
-#6_Post-processing: SNP filtering, annotation
-
 #!/bin/bash
 #SBATCH --verbose
-#SBATCH -c 14
+#SBATCH --mem=20G
+#SBATCH -c 12
 #SBATCH -p all
 #SBATCH -J 6_Post-processing
-#SBATCH -t 48:00:00
-#SBATCH -o 
-#SBATCH -e 
+#SBATCH -t 0-48:00:00
+#SBATCH -o /data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/farmers/trial/log/6_Post-processing_%j.log
+#SBATCH -e /data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/farmers/trial/log/6_Post-processing_%j.err
 
 #module load & libraries
 module purge
 eval "$(conda shell.bash hook)"
 conda activate ngs
 
-#variables
-DAT=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/farmers/fastq
-REF=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/ref
-HOM=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/farmers
+#Submit command: sbatch 6_Post-processing.sh
 
-#code
+#Print the task ID
+cd "$SLURM_SUBMIT_DIR"
+echo "My SLURM_JOB_ID: " $SLURM_JOB_ID
+
+#Variables
+REF=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/ref
+HOM=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/farmers/trial
+
+#Code
 #Compress and index
-bgzip ${HOM}/combined.vcf
-tabix -p ${HOM}/combined.vcf.gz
+bgzip ${HOM}/vcf/combined.vcf
+tabix -p vcf ${HOM}/vcf/combined.vcf.gz
 
 #SNP filtering - vcftools
-vcftools --gzvcf ${HOM}/combined.vcf.gz --remove-indels --maf 0.05 --max-missing 0.75 --minQ 30 --minDP 4 --mac 1 --max-alleles 2 --recode --recode-INFO-all --out ${HOM}/output.SNPs.maf005.MM075.q30.d4.biallele
+vcftools --gzvcf ${HOM}/vcf/combined.vcf.gz --remove-indels --maf 0.05 --max-missing 0.75 --minQ 30 --minDP 4 --mac 1 --max-alleles 2 --recode --recode-INFO-all --out ${HOM}/vcf/output.SNPs.maf005.MM075.q30.d4.biallele
 
 #Compress and index
-bgzip ${HOM}/output.SNPs.maf005.MM075.q30.d4.biallele.vcf
-tabix -p ${HOM}/output.SNPs.maf005.MM075.q30.d4.biallele.vcf.gz
+bgzip ${HOM}/vcf/output.SNPs.maf005.MM075.q30.d4.biallele.vcf
+tabix -p vcf ${HOM}/vcf/output.SNPs.maf005.MM075.q30.d4.biallele.vcf.gz
 
 #Annotattion
 java -jar ~/snpEff/snpEff.jar build -gff3 -v IRGSP
-java -Xmx16g -jar ~/snpEff/snpEff.jar -v IRGSP ${HOM}/output.SNPs.maf005.MM075.q30.d4.biallele.vcf.gz > ${HOM}/output.SNPs.maf005.MM075.q30.d4.biallele.IRGSP.annotated.vcf.gz
+java -Xmx16g -jar ~/snpEff/snpEff.jar -v IRGSP ${HOM}/vcf/output.SNPs.maf005.MM075.q30.d4.biallele.vcf.gz > ${HOM}/vcf/output.SNPs.maf005.MM075.q30.d4.biallele.IRGSP.annotated.vcf.gz
