2e79831e782ecf5da3dc67adc3586f139e23b877
nknglee <50165480+nknglee@users.noreply.github.com>
2025-01-23 11:08:19 +0000

    Update 1_Pre-processing.sh
    Edit data directory

--- 1.2_Historical samples/1_Pre-processing.sh
+++ 1.2_Historical samples/1_Pre-processing.sh
@@ -21,7 +21,7 @@ cd "$SLURM_SUBMIT_DIR"
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID ${SAMPLE}
 
 #Variables
-DAT=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/herbarium/fastq
+DAT=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/herbarium/trial/fastq
 REF=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/ref
 HOM=/data/users_area/yky10kg/GREENrice/Cons_Gen/datasets/herbarium/trial
 SAMPLE=$(ls ${DAT}/*_1.fastq.gz | rev | cut -d "/" -f 1 | rev | cut -f 1 -d "_" | sed -n "${SLURM_ARRAY_TASK_ID}p")
