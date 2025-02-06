64e1e6d676e35b26f54cdcf088558d751f807802
nknglee <50165480+nknglee@users.noreply.github.com>
2025-02-06 13:39:39 +0000

    Update Slurm_submit.sh
    Added job ID for array jobs

--- Slurm_submit.sh
+++ Slurm_submit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#SBATCH -o /path/to/log/Slurm_Record_%j.log
-#SBATCH -e /path/to/log/Slurm_Record_%j.err
 
 # Output directory
 mkdir -p /path/to/HOM/fastqc
@@ -18,14 +16,15 @@ ARRAY_SIZE=$(ls /path/to/fastq/*_1.fastq.gz | wc -l)
 # Replace ARRAY_SIZE in the original script and generate modified script
 sed "s/ARRAY_SIZE/${ARRAY_SIZE}/" script.sh > Modified_script.sh
 
-# Submit the job using modified script
-sbatch ./Modified_script.sh
+# Submit the job using modified script and extract JOB_ID for Array job
+ARRAY_JOB_ID=$(sbatch ./Modified_script.sh | awk '{print $4}')
 
 #Submit command: sbatch ./Slurm_submit.sh
 
 # Job records
 cd "$SLURM_SUBMIT_DIR"
 echo "SLURM_JOB_ID: $SLURM_JOB_ID" >> /path/to/log/Slurm_Record.log
+echo "SLURM_JOB_ID for ARRAY_JOB: $ARRAY_JOB_ID" >> /path/to/log/Slurm_Record.log
 echo "Date: $(date)" >> /path/to/log/Slurm_Record.log
 echo "Array Size: $ARRAY_SIZE" >> /path/to/log/Slurm_Record.log
 echo "--------------------------------------------" >> /path/to/log/Slurm_Record.log
